3e4aa00a2ad6b0aeece6fc04cc12332cd3e20a02
Sabina <109144680+Srasulova@users.noreply.github.com>
2024-10-21 18:56:07 -0400

    update the img in hero area, add typing effect

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -1,23 +1,22 @@
+"use client"
+
 import Image from "next/image";
-// import heroBG from "../public/hero-bg.jpg"
-// import logoLight from "/SR logo white bg.png"
-import logoDark from "../../../public/SR logo dark bg.png"
-
-import heroBG from "../../../public/hero-bg.jpg"
-
+import logoDark from "../../../public/SR logo dark bg.png";
+import keyboardImg from "../../../public/keyboard.jpg";
+import useTypingEffect from "../hooks/useTypingEffect";
 
 export default function HeroArea() {
+    const text = "  I'm Sabina. I'm a Software Engineer."
+    const typedText = useTypingEffect(text, 100)
+
     return (
         <>
-            <div className="bg-gray-900">
+            <div className="">
                 <header className="absolute inset-x-0 top-0 z-50">
-                    <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
-                        <div className="flex lg:flex-1">
-                            <a href="#" className="-m-1.5 p-1.5">
-                                <span className="sr-only">Your Company</span>
-                                <Image className="h-48 w-auto" src={logoDark} alt="logo" />
-                            </a>
-                        </div>
+                    <nav className="flex items-center justify-between py-6 max-w-7xl mx-auto" aria-label="Global">
+                        <a href="#" className="-m-1.5 p-1.5">
+                            <Image className="h-14 w-auto" src={logoDark} alt="logo" />
+                        </a>
                         <div className="flex lg:hidden">
                             <button type="button" className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-400">
                                 <span className="sr-only">Open main menu</span>
@@ -27,23 +26,18 @@ export default function HeroArea() {
                             </button>
                         </div>
                         <div className="hidden lg:flex lg:gap-x-12">
-                            <a href="#" className="text-sm font-semibold leading-6 text-white">Product</a>
-                            <a href="#" className="text-sm font-semibold leading-6 text-white">Features</a>
-                            <a href="#" className="text-sm font-semibold leading-6 text-white">Marketplace</a>
-                            <a href="#" className="text-sm font-semibold leading-6 text-white">Company</a>
-                        </div>
-                        <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-                            <a href="#" className="text-sm font-semibold leading-6 text-white">Log in <span aria-hidden="true">&rarr;</span></a>
+                            <a href="#" className="text-sm font-semibold leading-6 text-white hover:text-fuchsia-300">About me</a>
+                            <a href="#" className="text-sm font-semibold leading-6 text-white hover:text-fuchsia-300">My tech stack</a>
+                            <a href="#" className="text-sm font-semibold leading-6 text-white hover:text-fuchsia-300">Projects</a>
+                            <a href="#" className="text-sm font-semibold leading-6 text-white hover:text-fuchsia-300">Contact me</a>
                         </div>
                     </nav>
-                    {/* <!-- Mobile menu, show/hide based on menu open state. --> */}
+
                     <div className="lg:hidden" role="dialog" aria-modal="true">
-                        {/* <!-- Background backdrop, show/hide based on slide-over state. --> */}
                         <div className="fixed inset-0 z-50"></div>
-                        <div className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-gray-900 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-white/10">
+                        <div className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-cyan-950 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-white/10">
                             <div className="flex items-center justify-between">
                                 <a href="#" className="-m-1.5 p-1.5">
-                                    <span className="sr-only">Your Company</span>
                                     <Image className="h-8 w-auto" src={logoDark} alt="logo" />
                                 </a>
                                 <button type="button" className="-m-2.5 rounded-md p-2.5 text-gray-400">
@@ -54,15 +48,12 @@ export default function HeroArea() {
                                 </button>
                             </div>
                             <div className="mt-6 flow-root">
-                                <div className="-my-6 divide-y divide-gray-500/25">
+                                <div className="-my-6 divide-y ">
                                     <div className="space-y-2 py-6">
-                                        <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white hover:bg-gray-800">Product</a>
-                                        <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white hover:bg-gray-800">Features</a>
-                                        <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white hover:bg-gray-800">Marketplace</a>
-                                        <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white hover:bg-gray-800">Company</a>
-                                    </div>
-                                    <div className="py-6">
-                                        <a href="#" className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-white hover:bg-gray-800">Log in</a>
+                                        <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base font-medium leading-7 text-white hover:bg-cyan-900">About me</a>
+                                        <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base font-medium leading-7 text-white hover:bg-cyan-900">My tech stack</a>
+                                        <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base font-medium leading-7 text-white hover:bg-cyan-900">Projects</a>
+                                        <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base font-medium leading-7 text-white hover:bg-cyan-900">Contact me</a>
                                     </div>
                                 </div>
                             </div>
@@ -71,33 +62,19 @@ export default function HeroArea() {
                 </header>
 
                 <div className="relative isolate overflow-hidden pt-14">
-                    <Image src={heroBG} alt="" className="absolute inset-0 -z-10 h-full w-full object-cover" />
-                    <div className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80" aria-hidden="true">
-                        <div className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-20 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]" style={{
-                            clipPath: 'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-                        }}></div>
-                    </div>
-                    <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-                        <div className="hidden sm:mb-8 sm:flex sm:justify-center">
-                            <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-400 ring-1 ring-white/10 hover:ring-white/20">
-                                Announcing our next round of funding. <a href="#" className="font-semibold text-white"><span className="absolute inset-0" aria-hidden="true"></span>Read more <span aria-hidden="true">&rarr;</span></a>
-                            </div>
-                        </div>
+                    <Image src={keyboardImg} alt="" className="absolute inset-0 -z-10 h-full w-full object-cover" />
+                    <div className="absolute inset-0 bg-black opacity-45"></div> {/* Dark tint layer */}
+                    <div className="mx-auto max-w-4xl py-32 sm:py-48 lg:py-56 relative z-10">
                         <div className="text-center">
-                            <h1 className="text-balance text-4xl font-bold tracking-tight text-white sm:text-6xl">Data to enrich your online business</h1>
-                            <p className="mt-6 text-lg leading-8 text-gray-300">Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.</p>
+                            <h1 className="text-balance text-4xl font-medium text-white sm:text-6xl">{typedText}</h1>
+                            <p className="mt-6 text-3xl leading-8 text-white">Code like an artist, debug like a warrior!</p>
                             <div className="mt-10 flex items-center justify-center gap-x-6">
-                                <a href="#" className="rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-400">Get started</a>
-                                <a href="#" className="text-sm font-semibold leading-6 text-white">Learn more <span aria-hidden="true">→</span></a>
+                                <a href="#" className="rounded-md bg-cyan-700 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-cyan-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-cyan-800">Download my resume</a>
                             </div>
                         </div>
-                    </div>
-                    <div className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]" aria-hidden="true">
-                        <div className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-20 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]" style={{ clipPath: 'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)' }}></div>
                     </div>
                 </div>
             </div>
-
         </>
-    )
+    );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,8 +25,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Source+Code+Pro:wght@200..900&display=swap"
+          rel="stylesheet"
+        />
+      </head>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased font-source-code-pro`}
       >
         {children}
       </body>
