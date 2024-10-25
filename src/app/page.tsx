60e99f2f1b49c027f6132d109c8880b882d35252
Sabina <109144680+Srasulova@users.noreply.github.com>
2024-10-25 16:19:46 -0400

    add projects component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,22 @@
 import AboutMe from "./components/aboutMe";
 import HeroArea from "./components/hero";
 import MyTechStack from "./components/myTeckStack";
+import Projects from "./components/projects";
 import Image from "next/image";
 import talkIsCheap from "../../public/talkIsCheapBG.jpg"
 
 
 export default function Home() {
+
   return (
     <div >
       <main>
         <HeroArea />
         <AboutMe />
         <MyTechStack />
+        <Projects />
 
-        <div className='w-screen h-2/5'> {/* Change this line */}
+        <div className='w-screen h-2/5'>
           <div className="relative isolate overflow-hidden">
             <Image src={talkIsCheap} alt="" className="absolute inset-0 -z-10 h-full w-full object-cover object-bottom blur-sm" />
             <div className="absolute inset-0 bg-black opacity-25"></div> {/* Dark tint layer */}
